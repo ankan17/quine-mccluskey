075399ca3f32a8c25e91f2d8811acb685f1fa5f6
Ankan Poddar <friendyankan@gmail.com>
2018-04-26 12:03:39 +0530

    Solve PEP8 violations

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import math
-from functools import reduce
 from itertools import combinations
 
 
 class QuineMccluskey():
 
-
     def __init__(self, input_function):
 
         self.input_function = input_function
-        self.literals = int(math.ceil(math.log(self.input_function[-1]+1,2)))
+        self.literals = int(math.ceil(math.log(self.input_function[-1]+1, 2)))
         self.minterms = self._convert_to_binary(self.input_function)
 
         self.groups = [[] for x in range(self.literals+1)]
@@ -22,26 +20,26 @@ class QuineMccluskey():
         self._solve_pi_chart()
         self.print_answer()
 
-
     def _convert_to_binary(self, input_function):
 
-        """Groups the minterms into binary form padded with 0's to the left according to the number of literals"""
+        """Groups the minterms into binary form padded with 0's to
+        the left according to the number of literals."""
 
         return ["{0:b}".format(x).zfill(self.literals) for x in input_function]
 
-    
     def _group_minterms(self):
 
-        """Groups the different minterms according to number of 1's"""
+        """Groups the different minterms according to number of 1's."""
 
         list(map(lambda x: self.groups[x.count('1')].append(x), self.minterms))
 
-
     def _find_prime_implicants(self):
 
-        """Finds all the prime implicants from the given minterms"""
+        """Finds all the prime implicants from the given minterms."""
 
-        implicant_table = [[{x: 0 for x in self.groups[i]} for i in range(self.literals+1)]]
+        implicant_table = [
+            [{x: 0 for x in self.groups[i]} for i in range(self.literals+1)]
+        ]
         cur_round = 1
 
         while len(implicant_table[-1]) != 1:
@@ -51,17 +49,26 @@ class QuineMccluskey():
 
             for i in range(len(cur_implicant_list) - 1):
                 cur_implicant_group = {}
-                for (x, y) in [(a, b) for a in cur_implicant_list[i].keys() for b in cur_implicant_list[i+1].keys()]:
-                    bit_comparison = [1 if x[j] != y[j] else 0 for j in range(self.literals)]
+                for (x, y) in [
+                    (a, b) for a in cur_implicant_list[i].keys()
+                    for b in cur_implicant_list[i+1].keys()
+                ]:
+                    bit_comparison = [
+                        1 if x[j] != y[j]
+                        else 0 for j in range(self.literals)
+                    ]
                     if bit_comparison.count(1) == 1:
                         pos = bit_comparison.index(1)
-                        cur_implicant_group.update({x[:pos] + '_' + x[pos+1:]: 0})
-                        cur_implicant_list[i][x], cur_implicant_list[i+1][y] = 1, 1
+                        cur_implicant_group.update(
+                            {x[:pos] + '_' + x[pos+1:]: 0}
+                        )
+                        (cur_implicant_list[i][x],
+                         cur_implicant_list[i+1][y]) = (1, 1)
                 next_implicant_list.append(cur_implicant_group)
 
             implicant_table.append(next_implicant_list)
             cur_round += 1
- 
+
         prime_implicants = []
         for implicant_list in implicant_table:
             for implicant_group in implicant_list:
@@ -71,32 +78,38 @@ class QuineMccluskey():
 
         return prime_implicants
 
-
     def _generate_pi_chart(self):
 
-        """Generates and returns the prime implicant chart"""
+        """Generates and returns the prime implicant chart."""
 
-        pi_chart = [[pi, [0 for pi in self.input_function]] for pi in self.prime_implicants]
+        pi_chart = [
+            [pi, [0 for pi in self.input_function]]
+            for pi in self.prime_implicants
+        ]
 
         for row in pi_chart:
             pi = row[0]
             values = [0]
             for j in range(self.literals):
                 if pi[j] == '1':
-                    values = list(map(lambda x: x+2**(self.literals-1-j), values))
+                    values = list(
+                        map(lambda x: x+2**(self.literals-1-j), values)
+                    )
                 elif pi[j] == '_':
-                    values += list(map(lambda x: x+2**(self.literals-1-j), values))
+                    values += list(
+                        map(lambda x: x+2**(self.literals-1-j), values)
+                    )
 
             for value in values:
                 row[1][self.input_function.index(value)] = 1
 
         return pi_chart
 
-
     def _find_essential_pi(self):
 
-        """Finds the essential prime implicants and removes them from the prime implicant chart.
-        Returns False if any essential prime implicants were found, True otherwise"""
+        """Finds the essential prime implicants and removes them
+        from the prime implicant chart. Returns False if any
+        essential prime implicants were found, True otherwise."""
 
         if self.prime_implicant_chart != []:
 
@@ -113,16 +126,21 @@ class QuineMccluskey():
                         pos = j
                 if r == 1:
                     columns_to_remove.append(i)
-                    if self.prime_implicant_chart[pos][0] not in essential_prime_implicants:
-                        essential_prime_implicants.append(self.prime_implicant_chart[pos][0])
+                    if (
+                        self.prime_implicant_chart[pos][0]
+                        not in essential_prime_implicants
+                    ):
+                        essential_prime_implicants.append(
+                            self.prime_implicant_chart[pos][0]
+                        )
 
             start, end = 0, len(self.prime_implicant_chart)
             while start < end:
                 row = self.prime_implicant_chart[start]
                 if row[0] in essential_prime_implicants:
                     for j in range(num_of_minterms):
-                        if row[1][j] == 1:
-                            if j not in columns_to_remove: columns_to_remove.append(j)
+                        if row[1][j] == 1 and j not in columns_to_remove:
+                            columns_to_remove.append(j)
                     self.prime_implicant_chart.remove(row)
                     end -= 1
                 else:
@@ -137,24 +155,31 @@ class QuineMccluskey():
 
         return essential_prime_implicants == []
 
-
     def _remove_dominating_columns(self):
 
-        """Removes dominating columns and returns a boolean indicating whether column dominance was exhibited"""
+        """Removes dominating columns and returns a boolean
+        indicating whether column dominance was exhibited."""
 
         flag = True
 
         if self.prime_implicant_chart != []:
 
-            columns = [[row[1][i] for row in self.prime_implicant_chart] for i in range(len(self.prime_implicant_chart[0][1]))]
+            columns = [
+                [row[1][i] for row in self.prime_implicant_chart]
+                for i in range(len(self.prime_implicant_chart[0][1]))
+            ]
             columns_to_remove = []
             for i in range(len(columns)):
                 for col_to_be_compared in columns[:i] + columns[i+1:]:
-                    if self._compare(columns[i], col_to_be_compared) == "Dominating":
+                    if (
+                        self._compare(columns[i], col_to_be_compared)
+                        == "Dominating"
+                    ):
                         if i not in columns_to_remove:
                             columns_to_remove.append(i)
             columns_to_remove.sort()
-            if columns_to_remove != []: flag = False
+            if columns_to_remove != []:
+                flag = False
             for row in self.prime_implicant_chart:
                 for col in columns_to_remove:
                     row[1].pop(col - columns_to_remove.index(col))
@@ -177,11 +202,11 @@ class QuineMccluskey():
 
         return flag
 
-
     def _remove_dominated_rows(self):
 
-        """Removes dominated rows and returns a boolean indicating whether row dominance was exhibited"""
-        
+        """Removes dominated rows and returns a boolean indicating
+        whether row dominance was exhibited."""
+
         flag = True
 
         if self.prime_implicant_chart != []:
@@ -190,15 +215,19 @@ class QuineMccluskey():
             rows_to_remove = []
             for i in range(len(rows)):
                 for row_to_be_compared in rows[:i] + rows[i+1:]:
-                    if self._compare(rows[i], row_to_be_compared) == "Dominated":
+                    if (
+                        self._compare(rows[i], row_to_be_compared)
+                        == "Dominated"
+                    ):
                         if i not in rows_to_remove:
                             rows_to_remove.append(i)
-            if rows_to_remove != []: flag = False
+            if rows_to_remove != []:
+                flag = False
             for row in rows_to_remove:
                 self.prime_implicant_chart.pop(row - rows_to_remove.index(row))
                 rows.pop(row - rows_to_remove.index(row))
 
-            # Uncomment the following section if you wish to remove identical rows
+            # Uncomment the following section to remove identical rows
 
             # start, end = 0, len(rows)
             # while start < end:
@@ -212,10 +241,10 @@ class QuineMccluskey():
 
         return flag
 
-
     def _compare(self, list1, list2):
 
-        """Helper method to compare two lists and tell if they are dominating, dominated or Incomparable"""
+        """Helper method to compare two lists and tell if they are
+        dominating, dominated or Incomparable."""
 
         index = {0: "Incomparable", 1: "Dominated", 2: "Dominating"}
         status = 0
@@ -239,25 +268,30 @@ class QuineMccluskey():
 
         return index[status]
 
-
     def _solve_pi_chart(self):
 
-        """Reduces the prime implicant chart by finding essential prime implicants, deleting dominating columns and
-        deleting dominated rows in this order, iterated until the prime implicant chart is empty or can not be further
-        reduced. If the prime implicant chart is still not empty after reduction, it is solved by Petrick's Method"""
+        """Reduces the prime implicant chart by finding essential prime
+        implicants, deleting dominating columns and deleting dominated
+        rows in this order, iterated until the prime implicant chart is
+        empty or can not be further reduced. If the prime implicant chart is
+        still not empty after reduction, it is solved by Petrick's Method."""
 
         reduced = False
         while (self.prime_implicant_chart != []) and not reduced:
             reduced = self._find_essential_pi()
             reduced = self._remove_dominating_columns() and reduced
             reduced = self._remove_dominated_rows() and reduced
-        
+
         if self.prime_implicant_chart != []:
-            columns = [[row[1][i] for row in self.prime_implicant_chart] for i in range(len(self.prime_implicant_chart[0][1]))]
+            columns = [
+                [row[1][i] for row in self.prime_implicant_chart]
+                for i in range(len(self.prime_implicant_chart[0][1]))
+            ]
             P = []
             for column in columns:
                 for i in range(len(column)):
-                    if column[i] == 1: column[i] = i+1
+                    if column[i] == 1:
+                        column[i] = i+1
                 P.append(list(filter(lambda x: x != 0, column)))
             final_expression = [[elem] for elem in P[0]]
             for i in range(1, len(P)):
@@ -268,27 +302,33 @@ class QuineMccluskey():
                             temp = exp + [elem]
                         else:
                             temp = exp
-                        if temp not in new_exp: new_exp.append(temp)
+                        if temp not in new_exp:
+                            new_exp.append(temp)
                 final_expression = new_exp
 
             for pair in combinations(final_expression, 2):
-                if set(pair[0]).issubset(pair[1]):
-                    if pair[1] in final_expression: final_expression.remove(pair[1])
-                elif set(pair[1]).issubset(pair[0]):
-                    if pair[0] in final_expression: final_expression.remove(pair[0])
+                if (set(pair[0]).issubset(pair[1]) and
+                        pair[1] in final_expression):
+                            final_expression.remove(pair[1])
+                elif (set(pair[1]).issubset(pair[0]) and
+                      pair[0] in final_expression):
+                            final_expression.remove(pair[0])
 
             min_cover_size = min([len(e) for e in final_expression])
             solutions = []
             for exp in final_expression:
                 if len(exp) == min_cover_size:
-                    solutions.append(self.solutions[0] + [self.prime_implicant_chart[e-1][0] for e in exp])
-            
-            self.solutions = solutions
+                    solutions.append(
+                        self.solutions[0] +
+                        [self.prime_implicant_chart[e-1][0] for e in exp]
+                    )
 
+            self.solutions = solutions
 
     def print_answer(self):
 
-        """Prints all the possible answers from the generated solutions using switching variables"""
+        """Prints all the possible answers from the generated solutions
+        using switching variables."""
 
         print("Following are the solutions to the given SOP function")
         variables = [chr(i) for i in range(65, 65+self.literals)]
@@ -306,10 +346,12 @@ class QuineMccluskey():
 
 
 if __name__ == "__main__":
-    
-    input_function = list(map(int, input("Enter the boolean SOP function: ").split()))
+
+    input_function = list(
+        map(int, input("Enter the boolean SOP function: ").split())
+    )
     input_function.sort()
 
     # Example: input_function = [0,2,5,6,7,8,10,12,13,14,15]
-    
+
     q = QuineMccluskey(input_function)
